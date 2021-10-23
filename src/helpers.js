e51eac2fb0686d01467575438b8772e800226662
Wandole <mchirez@gmail.com>
2021-10-23 19:23:27 +0200

    Display information of a todoItem when click on his title

--- src/helpers.js
+++ src/helpers.js
@@ -102,3 +102,89 @@ export const displayTitles = (parentNode, dataArray) => {
 
     return titleListElement;
 }
+
+export const cbSelectProject = (e, todoList, titleListElement, titleListDiv) => {
+
+    /* Get current project's name */
+    let currentProject = e.target.value;
+
+    /* Display title of item in the current project */
+    let titleList = todoList.getTitleList(currentProject); 
+    titleListElement = displayTitles(titleListDiv, titleList, "title-list");
+}
+
+export const cbClickList = (e, todoList, parentNode) => {
+    /* Cb fct when a title is clicked */
+
+    /* Get item key */
+    const itemKey = e.target.getAttribute("data-key");
+    
+    /* Get todo item */
+    const item = todoList.getTodoItem(itemKey);
+
+    /* Display item details in the main screen */
+    return displayTodoItem(item, parentNode);
+}
+
+const displayTodoItem = (item, parentNode) => {
+    /* Display item details in the main screen */ 
+
+    const itemFormDiv = addNode(parentNode, "div", "form-div");
+    const itemForm = addNode(itemFormDiv, "form");
+
+    const itemKey = createKeyForm(itemForm, item.key);
+    const itemTitle = createFormElement(itemForm, "short", item.title, "item-title");
+    const itemDescr = createFormElement(itemForm, "long", item.description, "item-descr");
+    const itemNote = createFormElement(itemForm, "textarea", item.notes, "item-notes");
+    const itemDueDate = createFormElement(itemForm, "date", item.dueDate, "item-date");
+    const itemProject = createFormElement(itemForm, "long", item.project, "item-project");
+    const itemPriority = createFormElement(itemForm, "number", item.priority, "item-priority");
+
+    return itemFormDiv;
+}
+
+const createFormElement = (parentNode, inputType, data, nodeId) => {
+/* Create an inupt field of the given type and add it to the given node */
+    let inputForm;
+    if (inputType == "short") {
+        inputForm = document.createElement("input");
+        inputForm.setAttribute("max", "20");
+    }
+    else if (inputType == "long") {
+        inputForm = document.createElement("input");
+        inputForm.setAttribute("max", "30");
+    }
+    else if (inputType == "textarea") {
+        inputForm = document.createElement("TEXTAREA");
+        inputForm.setAttribute("maxLength", "300");
+    }
+    else if (inputType == "textarea") {
+        inputForm= document.createElement("input");
+        inputForm.setAttribute("type", "date");
+    }
+    else if (inputType == "number") {
+        inputForm= document.createElement("input");
+        inputForm.setAttribute("type", "number");
+    }
+    else{
+        return;
+    }
+    inputForm.value = data;
+    inputForm.id = nodeId
+
+    parentNode.appendChild(inputForm);
+
+    return inputForm;
+}
+
+const createKeyForm = (parentNode, data) => {
+    /* Create an invisible field to contains the item's key */
+    const inputForm = document.createElement("input");
+    inputForm.value = data;
+    inputForm.style.display = "none";
+    inputForm.id = "item-key"
+
+    parentNode.appendChild(inputForm);
+
+    return inputForm;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 import { TodoList } from './TodoList';
 import { addItemToTodoList, removeItemToTodoList, updateTodoItem, initTodoList, clearTodoList} from './App';
-import { addNode, addButton, createList, addSelectMenu, displayTitles } from './helpers'
+import { addNode, addButton, cbClickList, addSelectMenu, displayTitles, cbSelectProject} from './helpers'
 
 /* Load todoList from Storage or a empty one if no datas */
 
@@ -26,7 +26,7 @@ projectTitle.innerText = currentProject;
 
 /* MAIN */
 const lateralMenu = addNode(main, "div", "lateral-menu");
-const todoListElement = addNode(main, "div", "todo-display");
+const todoItemElement = addNode(main, "div", "todo-display");
 
 /* MENU LATERAL */
 /* Display available project */
@@ -34,18 +34,19 @@ let arrayOfProject = todoList.getProjectsList();
 const selectMenuProject = addSelectMenu(lateralMenu, arrayOfProject, "project-select");
 const titleListDiv = addNode(lateralMenu, "div", "title-list-container");
 
+/* Initialize display of title: show all title */
+let titleList = todoList.getTitleList(""); 
+const titleListElement = displayTitles(titleListDiv, titleList);
+
 /* Listen to the menu to display to choosen project. */
 selectMenuProject.addEventListener("change", (e) => {
+    cbSelectProject(e, todoList, titleListElement, titleListDiv);
+});
 
-    /* Get current project's name */
-    currentProject = e.target.value;
-
-    /* Display title of item in the current project */
-    let titleList = todoList.getTitleList(currentProject); 
-    const titleListElement = displayTitles(titleListDiv, titleList, "title-list");
-
-    /* Listen for click on the titleListElement */
-    titleListElement.addEventListener("click");
+/* Listen for click on the titleListElement */
+titleListElement.addEventListener("click", e => {
+    todoItemElement.innerHTML = "";
+    cbClickList(e, todoList, todoItemElement);
 });
 
 
